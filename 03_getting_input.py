ad57b890e55c1416c88b7f5bdc678e64af549402
lopesv1152 <101606015+lopesv1152@users.noreply.github.com>
2022-03-21 13:19:08 +1300

    Add files via upload

--- 03_getting_input.py
+++ 03_getting_input.py
@@ -9,8 +9,9 @@ double = fav_num * 2
 half = fav_num / 2
 squared = fav_num * fav_num
 
+print()
 # Greet the user
 print("Hi {}, you favourite number is {}".format(username, fav_num))
-
+print()
 # Output the results of doubling, halving 
 # and squaring their favourite number
